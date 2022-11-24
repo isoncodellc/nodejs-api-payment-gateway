0369319f3634baaf312572027f5579c86e7e3bec
juanjosechiroque <juanjosechiroque@gmail.com>
2022-11-24 09:40:25 -0500

    fix: typo in node_env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import { PORT } from "./src/config.js";
+import { PORT, NODE_ENV } from "./src/config.js";
 import app from "./src/app.js";
 
 app.listen(PORT);
 
 console.log("Server running on port", PORT);
+console.log("Enviroment", NODE_ENV);
--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ import { config } from "dotenv";
 config({silent: process.env.NODE_ENV != "production"});
 
 export const PORT = process.env.PORT;
+export const NODE_ENV = process.env.NODE_ENV;
 export const STRIPE_PRIVATE_KEY = process.env.STRIPE_PRIVATE_KEY;
 export const CONEKTA_PRIVATE_KEY = process.env.CONEKTA_PRIVATE_KEY;
