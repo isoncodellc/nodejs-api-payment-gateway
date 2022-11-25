c1ef25b3a4c7e73be91e94c6960529f9ac2b90d1
juanjosechiroque <juanjosechiroque@gmail.com>
2022-11-25 11:06:08 -0500

    feat: receive parameters for conekta

--- src/conekta/controller.js
+++ src/conekta/controller.js
@@ -4,36 +4,42 @@ import { CONEKTA_PRIVATE_KEY } from "../config.js";
 conekta.api_key = CONEKTA_PRIVATE_KEY;
 conekta.api_version = "2.0.0";
 
-export const createOrder = async (req, res) => {
+export const createOrder = async (req, res, next) => {
+
+    let { customer_name, customer_email, reference, amount, currency, success_url, failure_url } = req.body;
 
 	try 
 	{
 		let customer = await conekta.Customer.create({
-			name: "Payment Link Name",
-			email: "test@test.com"
+			name: customer_name,
+			email: customer_email
 		});
 
 		customer = customer.toObject();
 
+        amount = amount * 100;
+
 		const orderCreated = await conekta.Order.create({
-			"currency": "MXN",
+			"currency": currency,
 			"customer_info": {
 				"customer_id": customer.id
 			},
 			"line_items": [{
-				"name": "BookingId #100001",
-				"unit_price": 3000,
+				"name": reference,
+				"unit_price": amount,
 				"quantity": 1
 			}],
 			"checkout": {
-				"type":"HostedPayment",
-				"success_url": "http://testredirect.com/success",
-				"failure_url": "http://testredirect.com/fail",
+				"type": "HostedPayment",
+				"success_url": success_url,
+				"failure_url": failure_url,
 				"allowed_payment_methods": ["card"],
 				"multifactor_authentication": false,
 				"redirection_time": 4,
 			}
 		});
+
+        console.log("orderCreated ::", orderCreated._json);
 
 		const result = {
 			checkout_id: orderCreated._json.checkout.id,
@@ -41,13 +47,10 @@ export const createOrder = async (req, res) => {
 			redirect_url: orderCreated._json.checkout.url
 		};
 
-		console.log("result ::", result);
-
 		res.json(result);
 
 	} catch (error) {
-		console.log("error ::", error);
-		res.send(error);
+        next(err);
 	}
 
 };
